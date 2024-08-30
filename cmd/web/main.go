2d26f9d3039efa5516a34fc041056fbbd33431dc
Sebastian Fernandez Ledesma <sfernandezledesma@gmail.com>
2024-08-30 17:07:17 -0300

    Movement between pages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,11 @@ type DataListGames struct {
 	OthersGames []string
 }
 
+type DataCurrentGame struct {
+	Name string
+	Page Page
+}
+
 type Book struct {
 	Pages map[string]Page
 }
@@ -33,42 +38,51 @@ var dataListGames = DataListGames{
 }
 
 var books = map[string]Book{
-	"ASD": {Pages: map[string]Page{"1": dataPage, "2": {}}},
+	"ASD": {Pages: map[string]Page{
+		"1": dataPage,
+		"2": {
+			"This is page 2",
+			[]Option{
+				{"Go back to page 1", "1"},
+			}}}},
 }
 
 var dataPage = Page{
 	"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 	[]Option{
-		{"asdadfgag", "2"},
-		{"aslgjalskj", "3"},
+		{"Go to page 2", "2"},
+		{"Go back to page 1", "1"},
 	},
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("rootHandler")
-	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl := template.Must(template.ParseFiles("assets/index.html"))
 	tmpl.ExecuteTemplate(w, "index", dataListGames)
 }
 
 func playHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("playHandler")
-	book := books[r.PathValue("bookName")]
-	tmpl := template.Must(template.ParseFiles("index.html"))
-	tmpl.ExecuteTemplate(w, "game", book.Pages["1"])
+	bookName := r.PathValue("bookName")
+	pageNumber := r.PathValue("pageNumber")
+	page := books[bookName].Pages[pageNumber]
+	data := DataCurrentGame{Name: bookName, Page: page}
+	tmpl := template.Must(template.ParseFiles("assets/index.html"))
+	tmpl.ExecuteTemplate(w, "game", data)
 }
 
 func badRouteHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("badRouteHandler")
 	w.Header().Set("HX-Retarget", "body")
 	w.WriteHeader(http.StatusNotFound)
-	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl := template.Must(template.ParseFiles("assets/index.html"))
 	tmpl.ExecuteTemplate(w, "notfound", nil)
 }
 
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", rootHandler)
-	r.Get("/play/{bookName}", playHandler)
+	r.Get("/play/{bookName}/{pageNumber}", playHandler)
 	r.NotFound(badRouteHandler)
 
 	log.Println("Server is starting...")
